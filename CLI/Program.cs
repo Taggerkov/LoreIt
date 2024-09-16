4aeccfd8541266a328061db994c99513f976f747
Taggerkov <taggerkov@pm.me>
2024-09-16 19:14:35 +0200

    LoreIt - 0.2 Preview HotFix

---
+++ CLI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using CLI.UI;
+using MemoryRepo;
+
+var init = new StringBuilder();
+init.AppendLine("Welcome to the LoreIt!").AppendLine("Using v0.2 // Logic: Local - Storage: In Memory")
+    .AppendLine($"Today is: {DateTime.Now.ToShortDateString()}").AppendLine("\nGit: https://github.com/Taggerkov/LoreIt")
+    .AppendLine("Use 'help' command to get a list of available commands.");
+Console.WriteLine(init.ToString());
+try {
+    var cliApp = new CliApp(new UserImpl(), new ChannelImpl(), new PostImpl(), new CommentImpl());
+    await cliApp.Run();
+}
+catch (Exception) {
+    Console.Write("An error occured while initializing the command line interface!\nClosing the application ...");
+    Thread.Sleep(2000);
+}
