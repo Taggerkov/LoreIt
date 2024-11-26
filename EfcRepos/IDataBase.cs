6e093c614143d1b088857a6e8809f90caab33a80
Taggerkov <taggerkov@pm.me>
2024-11-26 18:10:52 +0100

    LoreIt v0.7 - DB Preview

---
+++ EfcRepos/IDataBase.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfcRepos;
+
+public class IDataBase{
+    
+}
---
+++ EfcRepos/SqLite.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServerEntities;
+
+namespace EfcRepos;
+
+public class SqLite : DbContext {
+    public DbSet<Post> Posts => Set<Post>();
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Comment> Comments => Set<Comment>();
+    public DbSet<Channel> Channels => Set<Channel>();
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        optionsBuilder.UseSqlite("Data Source=app.db");
+    }
+}
